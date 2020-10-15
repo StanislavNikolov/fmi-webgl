6ffe52bfd87fc6d7c96ab6cb9244975ec334e460
Stanislav Ch. Nikolov <stanislav.ltb@gmail.com>
2020-10-15 16:46:37 +0300

    01-exercise

---
+++ 01-exercise/p04/init.js
@@ -0,0 +1,44 @@
+// get context
+const canvas = document.getElementById('main-canvas');
+const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+if(gl == null) {
+	alert("Couldn't load WebGL");
+}
+
+// load & compile vertex shader
+const vShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vShader, document.getElementById('vshader').innerText);
+gl.compileShader(vShader);
+if(!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) {
+	alert("Couldn't compile vertex shader");
+}
+
+// load & compile fragment shader
+const fShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fShader, document.getElementById('fshader').innerText);
+gl.compileShader(fShader);
+if(!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) {
+	alert("Couldn't compile fragment shader");
+}
+
+// link together
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vShader);
+gl.attachShader(shaderProgram, fShader);
+gl.linkProgram(shaderProgram);
+if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+	alert(gl.getProgramInfoLog(shaderProgram));
+}
+
+gl.useProgram(shaderProgram);
+
+// misc
+const resize = () => {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	render(); // should be defined already, for example from main.js
+}
+resize();
+window.addEventListener('resize', resize);
+
